8d1b66ccca4d58aaa2c42e013a94f0793e9fad11
BUITUANLINH183784 <linh.bt183784@sis.hust.edu.vn>
2021-05-22 14:35:19 +0700

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,32 @@
-import './App.css';
-import { MainContent } from './components/MainContent/';
+import "./App.css";
+import { MainContent } from "./components/MainContent/";
 // import NavBar from './components/NavBar';
 import { MenuBar } from "./components/MenuBar/";
 // import "./components/MenuBar"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { GlobalProvider } from "./context/GlobalState";
+import Entrance from "./components/Entrance/Entrance";
 
 function App() {
   return (
     <div className="App">
-      <GlobalProvider>
-        <MenuBar />
-        <MainContent />
-      </GlobalProvider>
+      <BrowserRouter>
+        <GlobalProvider>
+          <Switch>
+            <Route path="/login">
+              <Entrance context={"login"}/>
+            </Route>
+            <Route path="/signup">
+              <Entrance context={"signup"}/>
+            </Route>
+            <Route path="/">
+              <MenuBar />
+              <MainContent />
+            </Route>
+          </Switch>
+        </GlobalProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/Entrance/Entrance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Entrance.module.css'
+
+export default () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
