72f316e63e01e4fc821611bda844cf8d9634c804
BUITUANLINH183784 <linh.bt183784@sis.hust.edu.vn>
2021-05-24 09:08:50 +0700

    css error cannot mix module and regular

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import { MainContent } from "./components/MainContent/";
-// import NavBar from './components/NavBar';
 import { MenuBar } from "./components/MenuBar/";
-// import "./components/MenuBar"
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Entrance } from "./components/Entrance/";
 
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { GlobalProvider } from "./context/GlobalState";
-import Entrance from "./components/Entrance/Entrance";
 
 function App() {
   return (
@@ -15,10 +13,10 @@ function App() {
         <GlobalProvider>
           <Switch>
             <Route path="/login">
-              <Entrance context={"login"}/>
+              <Entrance context={"login"} />
             </Route>
             <Route path="/signup">
-              <Entrance context={"signup"}/>
+              <Entrance context={"signup"} />
             </Route>
             <Route path="/">
               <MenuBar />
--- frontend/src/components/NavGroup/NavGroup.js
+++ frontend/src/components/NavGroup/NavGroup.js
@@ -1,64 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./NavGroup.css";
+import styles from "./NavGroup.module.css";
+
+const Buttons = () => (
+  <div className="buttons">
+    <Link to="/login" className="button-login button">
+      Log In
+    </Link>
+    <Link to="/signup" className="button-signup button">
+      Sign Up
+    </Link>
+  </div>
+);
+
+const SVGs = () => (
+  <span className="group">
+    <span className="person-icon-wrapper">
+      <svg
+        viewBox="0 0 250 250"
+        xmlns="http://www.w3.org/2000/svg"
+        className="person-icon"
+      >
+        <g fill="inherit">
+          <path d="M146.8 142.6h-37.6c-31.1 0-56.5 25.3-56.5 56.5 0 5.2 4.2 9.4 9.4 9.4h131.8c5.2 0 9.4-4.2 9.4-9.4 0-31.2-25.3-56.5-56.5-56.5zM128 130.7c20.1 0 36.4-16.3 36.4-36.4v-9.4c0-20.1-16.3-36.4-36.4-36.4S91.6 64.8 91.6 84.9v9.4c0 20.1 16.3 36.4 36.4 36.4z"></path>
+        </g>
+      </svg>
+    </span>
+    <svg
+      className="dropdown-icon"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M14.17,9.35,10,13.53,5.83,9.35a.5.5,0,0,1,.35-.85h7.64a.5.5,0,0,1,.35.85"></path>
+    </svg>
+  </span>
+);
+
+const DropMenu = () => (
+  <div className={styles.dropMenu}>
+    <Link to="/login" className={styles.loginOption}>
+      <svg
+        className={styles.loginOptionSVG}
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g fill="inherit">
+          <path d="M15,2 L5,2 C4.447,2 4,2.447 4,3 L4,9 L9.586,9 L8.293,7.707 C7.902,7.316 7.902,6.684 8.293,6.293 C8.684,5.902 9.316,5.902 9.707,6.293 L12.707,9.293 C13.098,9.684 13.098,10.316 12.707,10.707 L9.707,13.707 C9.512,13.902 9.256,14 9,14 C8.744,14 8.488,13.902 8.293,13.707 C7.902,13.316 7.902,12.684 8.293,12.293 L9.586,11 L4,11 L4,17 C4,17.553 4.447,18 5,18 L15,18 C15.553,18 16,17.553 16,17 L16,3 C16,2.447 15.553,2 15,2"></path>
+        </g>
+      </svg>
+      <div className={styles.loginOptionText}>Log In / Sign Up</div>
+    </Link>
+  </div>
+);
 
 export default () => {
+  const [dropdown, setDropdown] = useState(false);
   return (
     <div className="nav-wrapper">
       <div className="nav-group">
-        <div className="buttons">
-          <Link
-            to="/login"
-            className="button-login button"
-          >
-            Log In
-          </Link>
-          <Link
-            to="/signup"
-            className="button-signup button"
-          >
-            Sign Up
-          </Link>
-        </div>
-        <div
-          id="email-verification-tooltip-id"
-          className="group"
-        >
-          <div
-            id="change-username-tooltip-id"
-            className="group"
-          ></div>
+        <Buttons />
+        <div id="email-verification-tooltip-id" className="group">
           <div className="header-user-dropdown">
             <button
               aria-expanded="false"
               aria-haspopup="true"
               id="USER_DROPDOWN_ID"
               className="dropdown-button"
+              onClick={() => {
+                dropdown ? setDropdown(false) : setDropdown(true);
+              }}
             >
-              <span className="group">
-                <span className="person-icon-wrapper">
-                  <svg
-                    viewBox="0 0 250 250"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="person-icon"
-                  >
-                    <g fill="inherit">
-                      <path d="M146.8 142.6h-37.6c-31.1 0-56.5 25.3-56.5 56.5 0 5.2 4.2 9.4 9.4 9.4h131.8c5.2 0 9.4-4.2 9.4-9.4 0-31.2-25.3-56.5-56.5-56.5zM128 130.7c20.1 0 36.4-16.3 36.4-36.4v-9.4c0-20.1-16.3-36.4-36.4-36.4S91.6 64.8 91.6 84.9v9.4c0 20.1 16.3 36.4 36.4 36.4z"></path>
-                    </g>
-                  </svg>
-                </span>
-                <svg
-                  className="dropdown-icon"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M14.17,9.35,10,13.53,5.83,9.35a.5.5,0,0,1,.35-.85h7.64a.5.5,0,0,1,.35.85"></path>
-                </svg>
-              </span>
+              <SVGs />
               <span className="detail">User account menu</span>
             </button>
           </div>
         </div>
+        {dropdown ? <DropMenu /> : ""}
       </div>
     </div>
   );
