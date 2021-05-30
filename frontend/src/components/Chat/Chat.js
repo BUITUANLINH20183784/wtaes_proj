3043a62c929ed04197a014895aeaae001cdfd47f
BUITUANLINH183784 <linh.bt183784@sis.hust.edu.vn>
2021-05-30 20:54:45 +0700

    for the love of god i just want to hide the menu

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -4,24 +4,38 @@ import styles from "./Chat.module.css";
 import { GlobalContext } from "../../context/GlobalState";
 
 const Chat = () => {
-  const { users, current_user, sendMessage } = useContext(GlobalContext)
-  const [index, setIndex] = useState(0)
-  if (!users) return null
-  if (!current_user.isAuthenticated) return <Redirect to="/login" />
+  const { users, current_user, sendMessage } = useContext(GlobalContext);
+  const [index, setIndex] = useState(null);
+  if (!users) return null;
+  if (!current_user.isAuthenticated) return <Redirect to="/login" />;
 
   return (
     <div className={styles.container}>
       <div className={styles.conversationContainer}>
         <div className={styles.conversationHeader}>Chat</div>
-        <ConversationList index={index} setIndex={setIndex} users={users} conversations={!current_user.user ? null : current_user.user.conversation} />
+        <ConversationList
+          index={index}
+          setIndex={setIndex}
+          users={users}
+          conversations={
+            !current_user.user ? null : current_user.user.conversation
+          }
+        />
       </div>
       <div className={styles.messageRegionContainer}>
         <nav className={styles.messageRegionHeaderContainer}>
-          <span className={styles.messageRegionHeader}>{!current_user.user ? null : current_user.user.username}</span>
+          <span className={styles.messageRegionHeader}>
+            {!current_user.user ? null : current_user.user.username}
+          </span>
         </nav>
-        <MessageRegion messages={null} />
+        <MessageRegion
+          users={users}
+          conversation={current_user.user.conversation.find(
+            (conversation) => conversation.userID === index
+          )}
+        />
         <div className={styles.paddingSeparator}></div>
-        <MessageInput sendMessage={sendMessage} />
+        <MessageInput sendMessage={!index ? null : sendMessage} />
       </div>
     </div>
   );
@@ -30,27 +44,51 @@ const Chat = () => {
 export default Chat;
 
 const ConversationList = ({ conversations, users, index, setIndex }) => {
-  const [counter, setCounter] = useState(-1)
+  // const [counter, setCounter] = useState(-1)
 
   return (
     <div className={styles.conversationListContainer}>
-      {conversations.map(conversation => <Link className={styles.conversationItem} key={function() {
-        setCounter(counter + 1);
-        return counter;
-      }()} style={counter === index ? {background: "rgb(237 239 241"}: null} onClick={setIndex(key)}>
-        <span className={styles.userAvatar}>
-          <i/>
-        </span>
-        <div className={styles.userPeak}>
-          <h4>{users.find(user => user._id === conversation.userID).username}</h4>
-          <div>{conversation.message[0].content}</div>
-        </div>
-      </Link>)}
+      {conversations.map((conversation) => (
+        <Link
+          className={styles.conversationItem}
+          key={conversation.userID}
+          style={
+            conversation.userID === index
+              ? { background: "rgb(237 239 241" }
+              : null
+          }
+          onClick={setIndex(conversation.userID)}
+        >
+          <span className={styles.userAvatar}>
+            <i />
+          </span>
+          <div className={styles.userPeak}>
+            <h4>
+              {users.find((user) => user._id === conversation.userID).username}
+            </h4>
+            <div>{conversation.message[0].content}</div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
-}
+};
 
-const MessageRegion = () => {
+const MessageRegion = ({ conversation, users }) => {
+
+  const EmptyMessageRegion = () => {
+    return (
+      <div className={styles.messageRegion}>
+        <div className={styles.messageList}>
+        </div>
+      </div>
+    )
+  }
+
+  // if (!conversation || !users) return null;
+  if (!conversation) return <EmptyMessageRegion />
+  const peer = users.find((user) => user._id === conversation.userID);
+
   const UserMeta = () => (
     <Link className={styles.userMeta}>
       <img
@@ -77,15 +115,21 @@ const MessageRegion = () => {
   return (
     <div className={styles.messageRegion}>
       <div className={styles.messageList}>
-        <UserMeta />
-        <Message />
+        {
+          conversation.message.map(message =>
+            <>
+              <UserMeta />
+              <Message />
+            </>
+          )
+        }
       </div>
     </div>
   );
 };
 
 const MessageInput = ({ sendMessage }) => {
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("");
 
   return (
     <div className={styles.messageInputContainer}>
@@ -100,14 +144,17 @@ const MessageInput = ({ sendMessage }) => {
           onChange={(e) => setMessage(e.target.value)}
         ></textarea>
       </div>
-      <button className={styles.sendButton} onClick={() => {
-        sendMessage(message);
-        setMessage("");
-      }}>
+      <button
+        className={styles.sendButton}
+        onClick={() => {
+          sendMessage(message);
+          setMessage("");
+        }}
+      >
         <svg viewBox="0 0 24 24">
           <path d="M22.6559 10.8L2.39994 0.65995C2.15389 0.529239 1.87278 0.47974 1.59687 0.518539C1.32096 0.557338 1.06441 0.682447 0.863944 0.87595C0.656797 1.06381 0.51403 1.3121 0.455877 1.58564C0.397725 1.85917 0.427136 2.14407 0.539944 2.39995L4.58394 12L0.539944 21.6C0.429918 21.8544 0.401998 22.1368 0.460075 22.4078C0.518153 22.6788 0.659334 22.925 0.863944 23.112C1.10992 23.3469 1.43582 23.4798 1.77594 23.484C1.99315 23.4934 2.20883 23.4436 2.39994 23.34L22.6559 13.2C22.8781 13.0879 23.0648 12.9164 23.1952 12.7045C23.3256 12.4927 23.3947 12.2487 23.3947 12C23.3947 11.7512 23.3256 11.5072 23.1952 11.2954C23.0648 11.0835 22.8781 10.912 22.6559 10.8Z"></path>
         </svg>
       </button>
     </div>
   );
-}
+};
--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,26 @@
-import React from "react";
-import "./SearchBar.css"
+import React, { useContext, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { GlobalContext } from "../../context/GlobalState";
+import "./SearchBar.css";
 
 export default () => {
+  const [dropdown, setDropdown] = useState(false);
+  const [text, setText] = useState("")
+  // const suggestions = useRef(null)
+  // const search = useRef(null)
+
+  // useEffect(() => {
+  //   document.addEventListener("click", (e) => {
+
+  //     suggestions
+  //   })
+  //   return () => {
+  //     cleanup
+  //   }
+  // }, [input])
+
   return (
-    <div className="wrapper">
+    <div className="wrapper" onBlur={() => setDropdown(false)}>
       <div className="searchbar">
         <label className="search-icon">
           <svg
@@ -14,10 +31,59 @@ export default () => {
             <path d="M15.59,13.91l2.78,2.69a1.25,1.25,0,1,1-1.74,1.8l-2.82-2.73a8,8,0,1,1,1.78-1.76ZM14.64,9.2A5.45,5.45,0,1,0,9.2,14.64,5.45,5.45,0,0,0,14.64,9.2Z"></path>
           </svg>
         </label>
-        <form action="/" autoComplete="off" method="get">
-          <input type="search" placeholder="Search" className="search-input"/>
-        </form>
+        {/* <form action="/" autoComplete="off" method="get"> */}
+        <input
+          type="search"
+          placeholder="Search"
+          className="search-input"
+          onFocus={() => setDropdown(true)}
+          // onBlur={(e) => {
+          //   if (suggestions?.current.contains(e.target)) return;
+            // setDropdown(false);
+          // }}
+          // ref={search}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        {/* </form> */}
+        {dropdown ? <Suggestions text={text} close={() => setDropdown(false)}/> : null}
       </div>
     </div>
+  );
+};
+
+const Suggestions = ({ text, close }) => {
+  const { users, communities } = useContext(GlobalContext);
+
+  return (
+    <div className="suggestions">
+      {communities.filter(community => community.name.indexOf(text) >= 0).map(community => <SuggestionItem key={community._id} community={community} close={close}/>)}
+      {users.filter(user => user.username.indexOf(text) >= 0).map(user => <SuggestionItem key={user._id} user={user} close={close}/>)}
+    </div>
+  );
+};
+
+const SuggestionItem = ({ user, community, close }) => {
+  if (community) return (
+    <Link className="suggestion" to={`/r/${community._id}`} onMouseDown={e => e.preventDefault()} onClick={close}>
+      <i className="suggestionIcon" />
+      <div className="suggestionDetail">
+        <div className="title">{community.name}</div>
+        <div>
+          <div className="members">{community.memberCount} members</div>
+        </div>
+      </div>
+    </Link>
+  );
+  if (user) return (
+    <Link className="suggestion" to={`/u/${user._id}`} onMouseDown={e => e.preventDefault()} onClick={close}>
+      <i className="suggestionIcon userIcon" />
+      <div className="suggestionDetail">
+        <div className="title">{user.username}</div>
+        {/* <div>
+          <div className="members">{" "}</div>
+        </div> */}
+      </div>
+    </Link>
   );
 };
